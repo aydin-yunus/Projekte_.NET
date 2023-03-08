1af9f3c2ec3f137e83604e7eb10bff79db709459
Yunus Aydin <yunus.aydin@msn.com>
2023-03-08 22:36:39 +0100

    08.03.23_Ev

---
+++ Produkte_08.03/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Book myBook = new Book()
+        {
+            NumberOfPages = 10,
+            ISBN = "1000000",
+            Titel = "Reading for Presidents"
+        };
+        ////Dosya olusturma ve yazma
+        //File.WriteAllText(@"D:\TestOrdner\Books.json", JsonSerializer.Serialize(book));//burda verilen konumda dosyamizi olusturduk
+
+        //Dosya acma ve okuma
+        StreamReader inFile = File.OpenText(@"D:\TestOrdner\Books.json");
+        Book myBooks = JsonSerializer.Deserialize<Book>(inFile.ReadToEnd());
+        Console.WriteLine(myBooks);
+
+
+
+        Console.ReadKey();
+    }
+}
+public class Book
+{
+    public string ISBN { get; set; }
+    public string Titel { get; set; }
+    public int NumberOfPages { get; set; }
+    public Book()
+    {
+        ISBN = string.Empty;
+        Titel = string.Empty;
+        NumberOfPages = 0;
+    }
+    public override string ToString()
+    {
+        return string.Format($"{Titel} ({ISBN}), {NumberOfPages} Seiten");
+    }
+}
