0a564f03af2425819c26bbdd941497df94bb181e
Yunus Aydin <yunus.aydin@msn.com>
2023-03-15 14:42:11 +0100

    15.03.23_Aka

---
+++ Services - 01 - Feiertage_15.03/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Services___01___Feiertage_15._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            int jahr;
+            do
+            {
+                Console.Write("Jahr: ");
+            } while (int.TryParse(Console.ReadLine(), out jahr) == false);
+
+            List<(int Nummer, string kürzel, string name)> bundesländer = new List<(int nummer, string kürzel, string name)>()
+            {
+                (1, "BW", "Baden - Württemberg"),
+                (2, "BY", "Bayern"),
+                (3, "BE", "Berlin"),
+                (4, "BB", "Brandenburg"),
+                (5, "HB", "Bremen"),
+                (6, "HH", "Hamburg"),
+                (7, "HE", "Hessen"),
+                (8, "MV", "Mecklenburg - Vorpommern"),
+                (9, "NI", "Niedersachsen"),
+                (10, "NW", "Nordrhein - Westfalen"),
+                (11, "RP", "Rheinland - Pfalz"),
+                (12, "SL", "Saarland"),
+                (13, "SN", "Sachsen"),
+                (14, "ST", "Sachsen - Anhalt"),
+                (15, "SH", "Schleswig - Holstein"),
+                (16, "TH", "Thüringen")
+            };
+
+            Console.WriteLine(string.Join(Environment.NewLine, bundesländer));
+
+            int auswahl;
+            do
+            {
+                Console.Write("Auswahl: ");
+            } while (int.TryParse(Console.ReadLine(), out auswahl) == false
+                     && auswahl < 1 || auswahl > 16);
+
+            string kürzel = bundesländer.Single(b => b.Nummer == auswahl).kürzel.ToLower();
+
+            // Beispiel https://get.api-feiertage.de/?years=2021&states=nw
+            string url = string.Format("https://get.api-feiertage.de/?years={0}&states={1}", jahr, kürzel);
+
+            Console.WriteLine("Feiertage aus {0} für das Jahr {1}", bundesländer.SingleOrDefault(b => b.Nummer == auswahl).name, jahr);
+
+            string message = client.GetStringAsync(url).Result;
+
+            ServiceAntwort feiertage = null;
+
+            try
+            {
+                feiertage = JsonSerializer.Deserialize<ServiceAntwort>(message);
+
+                Console.WriteLine(string.Join<Feiertage>(Environment.NewLine, feiertage.Feiertage));
+            }
+            catch
+            {
+                Console.WriteLine(feiertage.Additional_note);
+            }
+        }   
+    }
+    public class ServiceAntwort
+    {
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("feiertage")]
+        public Feiertage[] Feiertage { get; set; }
+
+        [JsonPropertyName("additional_note")]
+        public string Additional_note { get; set; }
+    }
+
+    public class Feiertage
+    {
+        public string date { get; set; }
+        public string fname { get; set; }
+        public string all_states { get; set; }
+        public string bw { get; set; }
+        public string by { get; set; }
+        public string be { get; set; }
+        public string bb { get; set; }
+        public string hb { get; set; }
+        public string hh { get; set; }
+        public string he { get; set; }
+        public string mv { get; set; }
+        public string ni { get; set; }
+        public string nw { get; set; }
+        public string rp { get; set; }
+        public string sl { get; set; }
+        public string sn { get; set; }
+        public string st { get; set; }
+        public string sh { get; set; }
+        public string th { get; set; }
+        public string comment { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0,-25} Datum: {1} ", fname, Convert.ToDateTime(date).ToShortDateString());
+        }
+    }
+
+}
---
+++ Tupel - 01 - Grundlagen_15.03/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Tupel___01___Grundlagen_15._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paarbildung();
+            Console.WriteLine("--------");
+            Reißverschluss();
+            Console.ReadKey();
+
+        }
+        static public void Paarbildung()
+        {
+
+            List<int> zahlen = new List<int> { 9, 42, 60, 33, 38, 7, 7, 11, 18 };
+            for (int i = 0; i < zahlen.Count; i++)
+            {
+                if (zahlen.Count % 2 == 0)
+                {
+                    var pair = (zahlen[i], zahlen[i + 1]);
+                    Console.WriteLine(pair);
+                    i++;
+                }
+                else
+                {
+                    zahlen.Remove(zahlen.Last());
+                    var pair = (zahlen[i], zahlen[i + 1]);
+                    Console.WriteLine(pair);
+                    i++;
+                }
+            }
+        }
+        public static void Reißverschluss()
+        {
+            List<int> list1 = new List<int> {21,65,6,1,14,58,56,38,18,2 };
+            List<int> list2 = new List<int> { 95,1,86,32,66,27,67,10,54,55,80 };
+            if (list1.Count<list2.Count)
+            {
+                for (int i = 0; i < list1.Count; i++)
+                {
+                    if (list1.Count % 2 == 0)
+                    {
+                        var pair = (list1[i], list2[i ]);
+                        Console.WriteLine(pair);
+                    }
+                    else
+                    {
+                        list1.Remove(list1.Last());
+                        var pair = (list1[i], list2[i ]);
+                        Console.WriteLine(pair);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < list2.Count; i++)
+                {
+                    if (list2.Count % 2 == 0)
+                    {
+                        var pair = (list2[i], list1[i ]);
+                        Console.WriteLine(pair);
+                    }
+                    else
+                    {
+                        list2.Remove(list2.Last());
+                        var pair = (list2[i], list1[i]);
+                        Console.WriteLine(pair);
+                    }
+                }
+            }
+
+        }
+    }
+}
