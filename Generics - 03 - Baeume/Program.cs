a390f11a8b6a39f5b70f484e46e5b9a4e681cd88
Yunus Aydin <yunus.aydin@msn.com>
2023-03-22 23:35:51 +0100

    ertert

---
+++ Generics - 03 - Baeume/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generics___03___Baeume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
